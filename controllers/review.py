51dc8e41dc77f3e4e26de446a9ead0152ffe0287
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-02-27 11:17:07 +0800

    Prepare User and Review Scraper

--- controllers/review.py
+++ controllers/review.py
@@ -7,3 +7,19 @@ from .base import BaseScraper
 class ReviewScraper(BaseScraper):
     def __init__(self):
         super().__init__()
+        self.init_checkpoint()
+
+    def init_checkpoint(self):
+        try:
+            with open('./data/reviews/checkpoint.json', 'r') as fp:
+                self.checkpoint = load(fp)
+        except FileNotFoundError:
+            self.checkpoint = {
+                'reviews':[], # Keep track of all scraped genres
+                'current': "", # Keep track of currently being scraped genre
+                'page':0 # Keep track of page
+            }
+
+    def save_checkpoint(self):
+        with open('./data/reviews/checkpoint.json', 'w') as fp:
+            dump(self.checkpoint, fp)
--- controllers/user.py
+++ controllers/user.py
@@ -7,3 +7,19 @@ from .base import BaseScraper
 class UserScraper(BaseScraper):
     def __init__(self):
         super().__init__()
+        self.init_checkpoint()
+
+    def init_checkpoint(self):
+        try:
+            with open('./data/users/checkpoint.json', 'r') as fp:
+                self.checkpoint = load(fp)
+        except FileNotFoundError:
+            self.checkpoint = {
+                'users':[], # Keep track of all scraped genres
+                'current': "", # Keep track of currently being scraped genre
+                'page':0 # Keep track of page
+            }
+
+    def save_checkpoint(self):
+        with open('./data/users/checkpoint.json', 'w') as fp:
+            dump(self.checkpoint, fp)
